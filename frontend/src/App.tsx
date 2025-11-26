67321941cb90f0f314e638c31d415cb19a25fe30
Hanna Drozhdzh <hanna.drozhdzh@gmail.com>
2025-11-26 19:24:16 +0200

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <p>This is test</p>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg_pink: "#C17D8C",
+        admin_pink: "#FFDCE5",
+        btn_pink: "#C7526E",
+        modal: "#FFF7F9",
+      },
+      fontFamily: {
+        base: ["DM Sans", "sans-serif"],
+        heading: ["Lato", "sans-serif"],
+        logo: ["RocknRoll One", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
