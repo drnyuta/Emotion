5c76dbbf1643e2e9fb98efa4fe391df347674a7e
Hanna Drozhdzh <hanna.drozhdzh@gmail.com>
2025-12-12 22:10:53 +0200

    setup docker

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
+import { client, connectDB } from "./database";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+app.get('/health', async (_req, res) => {
+  try {
+    await client.query('SELECT 1');
+    res.status(200).send('OKkk');
+  } catch (err) {
+    console.error('Healthcheck failed:', err);
+    res.status(500).send('Database connection failed');
+  }
+});
+
+connectDB().then(() => {
+  app.listen(5000, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+  });
 });
